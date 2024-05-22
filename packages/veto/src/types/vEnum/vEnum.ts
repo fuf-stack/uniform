3cd915675643eeaaf233bc806f400af3a400551b
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-05-22 18:27:44 +0200

    fix(veto): add type VEnumSchema

--- packages/veto/src/types/vEnum/vEnum.ts
+++ packages/veto/src/types/vEnum/vEnum.ts
@@ -1,6 +1,9 @@
+import type { ZodEnum } from 'zod';
+
 import { z } from 'zod';
 
 // eslint-disable-next-line prefer-destructuring
 export const vEnum = z.enum;
 
 export type VEnum = typeof vEnum;
+export type VEnumSchema<T extends [string, ...string[]]> = ZodEnum<T>;
