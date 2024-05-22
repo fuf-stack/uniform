2ccd2eeffa303c94b7e13b291b13d19cc35e7a8d
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-05-22 21:44:47 +0200

    fix(veto): add type anotation to discriminatedUnion

--- packages/veto/src/types/discriminatedUnion/discriminatedUnion.ts
+++ packages/veto/src/types/discriminatedUnion/discriminatedUnion.ts
@@ -1,6 +1,17 @@
+import type { ZodDiscriminatedUnionOption } from 'zod';
+
 import { z } from 'zod';
 
 // eslint-disable-next-line prefer-destructuring
-export const discriminatedUnion = z.discriminatedUnion;
+export const discriminatedUnion: <
+  Discriminator extends string,
+  Types extends [
+    ZodDiscriminatedUnionOption<Discriminator>,
+    ...ZodDiscriminatedUnionOption<Discriminator>[],
+  ],
+>(
+  discriminator: Discriminator,
+  options: Types,
+) => z.ZodDiscriminatedUnion<Discriminator, Types> = z.discriminatedUnion;
 
 export type VDiscriminatedUnion = typeof discriminatedUnion;
