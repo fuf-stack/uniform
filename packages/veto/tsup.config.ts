b82220b76f8a97746e7b02f0e115482b940415b7
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-05-22 12:55:53 +0200

    fix(veto): use dts-bundle-generator to generate types bundle

--- packages/veto/tsup.config.ts
+++ packages/veto/tsup.config.ts
@@ -3,13 +3,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
-  // INFO: we have added zod as a devDep and run tsup with --dts-resolve
-  // in order to resolve the zod types to fix:
-  // The inferred type of * cannot be named without a reference to */node_modules/zod .
-  // This is likely not portable. A type annotation is necessary.
-  // see: https://github.com/microsoft/TypeScript/issues/47663
-  // see: https://github.com/quadristan/ts-indirect-type-reference-bug
-  dts: true,
+  // we use dts-bundle-generator to generate types (see dtsbundle.config.js)
+  dts: false,
   entry: ['src', '!src/**/*.test.*'],
   format: ['cjs', 'esm'],
   sourcemap: true,
