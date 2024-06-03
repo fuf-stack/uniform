026aa173d6b9f4844e92e5484faea065c32b70c2
Oscar Kemme <35099688+fighter3005@users.noreply.github.com>
2024-06-03 09:01:24 +0200

    fix(pixels): accordion required props

--- packages/pixels/src/Accordion/Accordion.tsx
+++ packages/pixels/src/Accordion/Accordion.tsx
@@ -8,15 +8,15 @@ import {
 
 export interface AccordionProps {
   /** Props for AccordionItems */
-  accordionItems?: AccordionItemProps[];
+  accordionItems: AccordionItemProps[];
   /** CSS class name */
   className?: string;
   /** Array of keys for the AccordionItem(s) to be expanded by default */
-  defaultSelectedKeys: undefined | 'all' | Iterable<number | string>;
+  defaultSelectedKeys?: undefined | 'all' | Iterable<number | string>;
   /** Disables the Accordion */
   disabled?: boolean;
   /** Array of keys for the AccordionItems to disable */
-  disabledKeys: Iterable<number | string>;
+  disabledKeys?: Iterable<number | string>;
   /** Force always one AccordionItem to be open. */
   disallowEmptySelection?: boolean;
   /** props for styling the Divider */
@@ -24,9 +24,9 @@ export interface AccordionProps {
   /** Hide the expanded/collapsed indicator icon */
   hideIndicator?: boolean;
   /** Callback function for when a Accordion Item is expanded or collapsed */
-  onSelectionChange: (keys: 'all' | Iterable<number | string>) => unknown;
+  onSelectionChange?: (keys: 'all' | Iterable<number | string>) => unknown;
   /** Set whether multiple or only a single AccordionItems can be expanded */
-  selectionMode: 'single' | 'multiple';
+  selectionMode?: 'single' | 'multiple';
   /** enable or disable the divider between each AccordionItem */
   showDivider?: boolean;
   /** style variant of the Accordion */
