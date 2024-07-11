9b7b4367f4bc4ee7d43f25b3b2492bb00c204885
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-07-11 15:49:52 +0200

    feat(uniform): add initial FieldArray component (#277)
    * feat(uniform): add initial FieldArray component
    
    * fix(uniform): export FieldArray from package
    
    * feat(uniform): add FieldArray test (snapshots)
    
    * fix(uniform): final refactorings in FieldArray
    
    ---------
    
    Co-authored-by: Oscar Kemme <35099688+fighter3005@users.noreply.github.com>

---
+++ packages/uniform/src/FieldArray/index.ts
@@ -0,0 +1,11 @@
+import FieldArray from './FieldArray';
+
+export type {
+  FieldArrayProps,
+  FieldArrayHideOption,
+  FieldArrayFieldChildren,
+} from './FieldArray';
+
+export { FieldArray };
+
+export default FieldArray;
--- packages/uniform/src/index.ts
+++ packages/uniform/src/index.ts
@@ -1,3 +1,4 @@
+export * from './FieldArray';
 export * from './Form';
 export * from './Grid';
 export * from './helpers';
