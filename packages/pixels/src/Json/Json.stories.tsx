08c582d6f2554ec082e1c9c16ff8de0c78205c80
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-12-17 16:53:22 +0100

    chore(pixels): add additional Json story

--- packages/pixels/src/Json/Json.stories.tsx
+++ packages/pixels/src/Json/Json.stories.tsx
@@ -39,3 +39,36 @@ export const InvalidJson: Story = {
     value: 'this is a string, not a Json',
   },
 };
+
+const programmersLife = {
+  coffee: {
+    cups: 42,
+    status: 'Not enough',
+    lastBreak: "What's a break?",
+    type: "Whatever's left in the pot",
+  },
+  bugs: {
+    found: 99,
+    fixed: 1,
+    created: 157,
+    excuses: [
+      'It works on my machine',
+      "That's not a bug, it's a feature",
+      'Cannot reproduce',
+      null, // The bug that got away
+    ],
+  },
+  meetings: {
+    scheduled: 8,
+    necessary: 0,
+    timeSpentPretendingToPayAttention: '3 hours',
+    cameraMalfunction: true,
+    muted: 'Definitely not talking about the project manager',
+  },
+};
+
+export const DayInTheLifeOfAProgrammer: Story = {
+  args: {
+    value: programmersLife,
+  },
+};
